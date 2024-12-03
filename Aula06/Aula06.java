9bc1a7c1e1cae3ec60be168fc1b135fadb1b004a
Kayc Henderson <146909378+kaychenderson@users.noreply.github.com>
2024-12-02 21:37:00 -0300

    Aula 06 - POO em Java

---
+++ Aula06/Aula06.java
@@ -0,0 +1,17 @@
+public class Aula06 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       ControleRemoto c = new ControleRemoto();
+       c.ligar();
+       c.maisVolume();
+       //c.ligarMudo();
+       c.play();
+       
+       c.abrirMenu();
+       c.fecharMenu();
+    }
+    
+}
