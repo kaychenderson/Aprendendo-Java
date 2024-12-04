26ec860a6a85f978b1e46cc3de1110d89515b754
Kayc Henderson <146909378+kaychenderson@users.noreply.github.com>
2024-12-03 22:38:03 -0300

    Aula 02 - POO em Java

--- Aula02/Caneta.java
+++ Aula02/Caneta.java
@@ -18,7 +18,7 @@ public class Caneta {
 
     void rabiscar() {
         if (this.tampada == true) {
-            System.out.println("ERRO! Não posso rabiscar.");
+            System.out.println("[ERRO!] Não posso rabiscar.");
         } else {
             System.out.println("Estou rabiscando.");
         }
